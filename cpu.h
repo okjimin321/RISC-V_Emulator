99d7d27ca6ecd042243348236bd37fbe0956c94e
okjimin321 <okjimin321@gmail.com>
2026-04-13 00:54:43 +0900

    add TIMER Address

--- cpu.h
+++ cpu.h
@@ -9,12 +9,15 @@ private:
 
     uint32_t regs[32];
     uint32_t pc;
+
+    // TODO (Seperate CPU/BUS/MEMORY)
     std::vector<uint8_t> memory;
     std::vector<uint32_t> frameBuffer;
     
 public:
     static constexpr uint32_t UART_ADDR     = 0x10000000;
     static constexpr uint32_t FRAMEBUF_ADDR = 0x20000000;
+    static constexpr uint32_t TIMER_ADDR    = 0x30000000; // TODO
 
     RISCV_CPU();
     uint32_t fetch();

d8e8c6db310f78112e81ca6ef12848433f3e9a9c
okjimin321 <okjimin321@gmail.com>
2026-04-13 00:08:35 +0900

    add Frame Buffer

--- cpu.h
+++ cpu.h
@@ -6,15 +6,16 @@
 class RISCV_CPU{
 private:
     static constexpr uint32_t ENTRY_ADDR  = 0;
-    
 
     uint32_t regs[32];
     uint32_t pc;
     std::vector<uint8_t> memory;
+    std::vector<uint32_t> framebuffer;
 
 public:
-    volatile static constexpr uint32_t UART_ADDR = 0x10000000;
-    
+    static constexpr uint32_t UART_ADDR     = 0x10000000;
+    static constexpr uint32_t FRAMEBUF_ADDR = 0x20000000;
+
     RISCV_CPU();
     uint32_t fetch();
     void execute(uint32_t);

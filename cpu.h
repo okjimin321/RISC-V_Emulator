72cf58cca27603cf2d43dbbdeef788bcb4a3b638
okjimin321 <okjimin321@gmail.com>
2026-04-12 23:44:50 +0900

    add MMIO Address(UART)

--- cpu.h
+++ cpu.h
@@ -6,11 +6,15 @@
 class RISCV_CPU{
 private:
     static constexpr uint32_t ENTRY_ADDR  = 0;
+    
+
     uint32_t regs[32];
     uint32_t pc;
     std::vector<uint8_t> memory;
 
 public:
+    volatile static constexpr uint32_t UART_ADDR = 0x10000000;
+    
     RISCV_CPU();
     uint32_t fetch();
     void execute(uint32_t);

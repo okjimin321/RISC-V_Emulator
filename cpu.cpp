9d653e1360350d005b3ad0202cda62750a13b724
okjimin321 <okjimin321@gmail.com>
2026-04-12 23:45:20 +0900

    add M extension such as Mul and div

--- cpu.cpp
+++ cpu.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 #include "cpu.h"
 
 RISCV_CPU::RISCV_CPU(): pc { ENTRY_ADDR }, memory(1024 * 1024, 0) {
@@ -74,9 +75,90 @@ void RISCV_CPU::execute(uint32_t inst){
             uint8_t funct3 = (inst >> 12) & 0x07;
             uint8_t rs1    = (inst >> 15) & 0x1f;
             uint8_t rs2    = (inst >> 20) & 0x1f;
-            uint8_t funct7 = (inst >> 25) & 0x7f;
+            uint8_t funct7 = static_cast<uint8_t>(inst >> 25) & 0x7f;
 
             uint32_t result = 0;
+
+            if(funct7 == 0x01){ // M extension
+                
+                switch(funct3){
+                    case 0x00:{ // MUL 
+                        result = regs[rs1] * regs[rs2];
+                        break;
+                    }
+                    case 0x01: { // MULH
+                        int64_t a = static_cast<int64_t>(static_cast<int32_t>(regs[rs1]));
+                        int64_t b = static_cast<int64_t>(static_cast<int32_t>(regs[rs2]));
+                        result = (a * b) >> 32;
+                        break;
+                    }
+                    case 0x02:{ // MULHSU
+                        int64_t a = static_cast<int64_t>(static_cast<int32_t>(regs[rs1]));
+                        int64_t b = static_cast<int64_t>(regs[rs2]);
+                        result = (a * b) >> 32; 
+                        break;
+                    } 
+                    case 0x03:{ // MULHU
+                        uint64_t a = static_cast<uint64_t>(regs[rs1]);
+                        uint64_t b = static_cast<uint64_t>(regs[rs2]); 
+                        result = (a * b) >> 32;
+                        break;
+                    }
+                    case 0x04:{ // DIV
+                        int32_t a = static_cast<int32_t>(regs[rs1]);
+                        int32_t b = static_cast<int32_t>(regs[rs2]);
+
+                        if(b == 0)
+                            result = 0xffffffff;
+                        else if(a == INT32_MIN && b == -1)
+                            result = static_cast<uint32_t>(INT_MIN); 
+                        else
+                            result = a / b;
+                        
+                        break;
+                    }
+                    case 0x05:{ // DIVU
+                        uint32_t a = regs[rs1];
+                        uint32_t b = regs[rs2];
+
+                        if(b == 0) 
+                            result = 0xffffffff;
+                        else 
+                            result = a / b;
+
+                        break;
+                    }
+                    case 0x06:{ // REM
+                        int32_t a = static_cast<int32_t>(regs[rs1]);
+                        int32_t b = static_cast<int32_t>(regs[rs2]);
+
+                        if(b == 0)
+                            result = a;
+                        else if(a == INT32_MIN && b == -1)
+                            result = 0;
+                        else 
+                            result = a % b;
+                        
+                        break;
+                    }
+                    case 0x07:{ // REMU
+                        uint32_t a = regs[rs1];
+                        uint32_t b = regs[rs2];
+
+                        if(b == 0)
+                            result = a;
+                        else 
+                            result = a % b;
+
+                        break;
+                    }        
+                }
+
+                if(rd != 0)
+                    regs[rd] = result;
+                    
+                break;
+            }
 
             switch (funct3){
                 case 0x00:{ // add and sub
@@ -284,6 +366,12 @@ void RISCV_CPU::execute(uint32_t inst){
                     result = (regs[rs2] & 0xffffffff);
                     break;
                 }
+            }
+
+            if(addr == UART_ADDR){ // MMIO
+                std::cout << static_cast<char>(result & 0xff);
+                std::cout.flush();
+                return;
             }
 
             for(int i = 0; i < (1 << funct3); i++){
